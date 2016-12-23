d218b04e181ccaaabe05dca81b4eafee1429a18c
fc-bschumacher <Bastian.Schumacher@fincon.eu>
2016-12-23 15:32:15 +0100

    refactored

--- src/bullshitbingo/DefaultSettings.java
+++ src/bullshitbingo/DefaultSettings.java
@@ -3,18 +3,27 @@ package bullshitbingo;
 //DEFAULT SETTINGS RESOURCE FILE
 public class DefaultSettings {
 	
+	//default game configuration
+	public GameConfiguration getGameConfiguration() {
+		GameConfiguration gameConfiguration = new GameConfiguration();
+		gameConfiguration.buzzwords = getBuzzwords();
+		gameConfiguration.separator = getSeparator();
+		gameConfiguration.numberOfRows = getNumberOfRows();
+		return gameConfiguration;
+	}
+	
 	//default buzzword list
-	public String getBuzzwords() {
-		return "Sonderlocke,ergebnisorientiert,Stakeholder,Teilprojektleiter,annahmenbasiert,pragmatisch,Tiefenbohrung,Wording,Optimierungsbedarf,Akzeptanzkriterien,Forschungsauftrag,Durchstich,streamlinen,aufgleisen,Klärungsbedarf,Druck machen,Cloud Competence Center/ CCC,Quick Win,greenlighten,Wissenstransfer,Bullet Points,BiPRO Norm,Prototyp,On Hold,Enabler,onboarden,Wackelpudding,proaktiv,Eckpfeiler,Leitplanken,in die Spur bringen,aufgehübscht,Senior,Junior,Reminder";
+	private String getBuzzwords() {
+		return "Sonderlocke,ergebnisorientiert,Mehrwert schaffen,(Software/Updates) einspielen,Stakeholder,Teilprojektleiter,annahmenbasiert,pragmatisch,Tiefenbohrung,Wording,Optimierungsbedarf,Akzeptanzkriterien,Forschungsauftrag,Durchstich,streamlinen,aufgleisen,Klärungsbedarf,Druck machen,Cloud Competence Center/ CCC,Quick Win,greenlighten,Wissenstransfer,Bullet Points,BiPRO Norm,Prototyp,On Hold,Enabler,onboarden,Wackelpudding,proaktiv,Eckpfeiler,Leitplanken,in die Spur bringen,aufgehübscht,Senior,Junior,Reminder";
 	}
 	
 	//default separator character for buzzword list
-	public char getSeparator() {
+	private char getSeparator() {
 		return ',';
 	}
 	
 	//default number of rows
-	public int getNumberOfRows(){
+	private int getNumberOfRows(){
 		return 5;
 	}
 }
